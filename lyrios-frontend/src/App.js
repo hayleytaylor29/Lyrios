6a4df0d8e33852196afc767bf6bcadc6fccf914d
hayleytaylor29 <hft929@gmail.com>
2021-06-26 12:55:21 -0400

    moved netlify file into frontend

--- lyrios-frontend/src/App.js
+++ lyrios-frontend/src/App.js
@@ -6,8 +6,81 @@ import Home from './pages/index';
 import Session from './pages/session';
 import Show from './pages/show';
 import User from './pages/user';
-import Edit from './pages/edit'
 
+const Nav = styled.nav`
+// background: #6B6B6B;
+background: black;
+height: 100px;
+display: flex;
+justify-content: space-between;
+padding: 0.4rem calc((100vw - 1000px) / 2);
+z-index: 12;
+`;
+
+const NavLink = styled(Link)`
+// color: #808080;
+color: white;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0 1rem;
+height: 100%;
+cursor: pointer;
+&.active {
+	color: #28ACEA;
+}
+`;
+
+const Bars = styled(FaBars)`
+display: none;
+color: #808080;
+@media screen and (max-width: 768px) {
+	display: block;
+	position: absolute;
+	top: 0;
+	right: 0;
+	transform: translate(-100%, 75%);
+	font-size: 1.8 rem;
+	cursor: pointer;
+}
+`;
+
+const NavMenu = styled.div`
+display: flex;
+align-items: center;
+margin-right: -24px;
+@media screen and (max-width: 768px) {
+	display: none;
+}
+`;
+
+const NavBtn = styled.nav`
+display: flex;
+align-items: center;
+margin-right: 24px;
+@media screen and (max-width: 768px) {
+	display: none;
+}
+`;
+
+const NavBtnLink = styled(Link)`
+border-radius: 4px;
+background: #808080;
+padding: 10px 22px;
+color: #000000;
+outline: none;
+border: none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration: none;
+/* Second Nav */
+margin-left: 24px;
+&:hover {
+	transition: all 0.2s ease-in-out;
+	background: #fff;
+	color: #808080;
+}
+`;
 
 function App() {
 
@@ -25,9 +98,6 @@ function App() {
           </Route>
           <Route path='/user'>
             <User/>
-          </Route>
-          <Route path = '/edit'>
-          <Edit/>
           </Route>
           <Route path='/'>
             <Home/>
